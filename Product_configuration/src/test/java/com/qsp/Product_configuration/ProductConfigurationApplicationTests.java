946cbeefe99940ae66b774bf9949d4a4b4f464ca
M ARSHAD <138919086+ARSHADkorak@users.noreply.github.com>
2025-07-12 20:49:32 +0530

    Add files via upload

---
+++ Product_configuration/src/test/java/com/qsp/Product_configuration/ProductConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsp.Product_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Product_configurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
